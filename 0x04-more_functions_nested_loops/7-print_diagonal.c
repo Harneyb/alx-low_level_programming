81243c82a7d88a1669aec73107cb338a0c4f8011
Harneyb <nworguhope128@gmail.com>
2023-03-24 20:32:04 -0700

    father

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -11,15 +11,14 @@ int i;
 int j;
 
 if (n <= 0)
-{
 _putchar ('\n');
-{
 for (i = 0; i < n; i++)
 {
-_putchar('_');
+for (j = 0; j < i; j++)
+{
+_putchar(' ');
 }
 _putchar('\\');
 _putchar('\n');
-}
 }
 }
